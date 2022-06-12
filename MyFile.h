7b0560a14caf726b4bed14ed93df649ebf23b955
Nikita Toropov <62467316+tossik8@users.noreply.github.com>
2022-06-12 12:51:28 +0200

    Add files via upload

---
+++ MyFile.h
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+/*!MyFile class is needed for storing information
+* about the passwords and categories while user is running the program
+*/
+class MyFile {
+	std::string text;
+	std::vector<std::string> category;
+public:
+	MyFile();
+	void categories() const;
+	void setText(std::string text);
+	std::string getText();
+	void addPassword();
+	void addCategory(std::string cate);
+	void removeCategory();
+	void save(std::string file, std::string key);
+	std::string chooseCat();
+	void editPassword();
+	void removePassword();
+	void searchPassword();
+};
+int num_of_char();

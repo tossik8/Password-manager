0db2af3cb36db4f6995a8e29549fb5c3389cec0e
Nikita Toropov <62467316+tossik8@users.noreply.github.com>
2022-06-19 15:02:59 +0200

    Update recordscompare.h

--- recordscompare.h
+++ recordscompare.h
@@ -10,6 +10,6 @@ public:
 	/// @param s1 is the first string which is to be compared
 	/// @param s2 is the second string which is to be compared
 	/// @return true - if they shouldn't be swapped, false otherwise
-	bool operator()(const std::string& s1, const std::string& s2);
+	bool operator()(const std::string& s1, const std::string& s2) const;
 };
 
